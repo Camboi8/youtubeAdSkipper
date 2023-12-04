0d669ec489983cf70f89d4abd84125a65ab783f6
Camboi8 <137013856+Camboi8@users.noreply.github.com>
2023-12-03 21:42:21 -0800

    updated youtube.js
    added comments, removed "var code = event.code"

--- youtube.js
+++ youtube.js
@@ -1,19 +1,18 @@
 const site = window.location.hostname
 
-if (site.includes("youtube.com")){
+if (site.includes("youtube.com")){ //easy check for whether or not program
     alert("slay queen");
 }
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', (event) => { //adds something that waits to see if a key is pressed
     var name = event.key;
-    var code = event.code;
 
-    if (name === 'e') {
+    if (name === 'e') { //runs when press 'e' key
         document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate=12;
     }
-    else if (name === 'r') {
+    else if (name === 'r') { //runs when press 'r' key
         document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate=1;
     }
-    else if (name === 'y') {
+    else if (name === 'y') { //runs when press 'y' key
         document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate=2;
     }
 }, false);
