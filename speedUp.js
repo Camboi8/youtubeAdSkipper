21aff0ba3d77501e284931026d6a6ee75dc5e210
Camboi8 <137013856+Camboi8@users.noreply.github.com>
2024-02-05 19:29:53 -0800

    Update and rename youtube.js to speedUp.js

---
+++ speedUp.js
@@ -0,0 +1,18 @@
+const site = window.location.hostname
+
+if (site.includes("youtube.com")){
+    alert("slay queen");
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+    
+        if ((name === 'e') || (name === 'E')) {
+            document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate=12;
+        }
+        else if ((name === 'r') || (name === 'R')) {
+            document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate=1;
+        }
+        else if ((name === 'y') || (name === 'Y')) {
+            document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate=2;
+        }
+    }, false);
+}
